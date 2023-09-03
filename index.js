1b6b9219448d8b8942ca31fdb66cc33f22dfdc5d
PANKAJ RAJPUT <thakurrajputpankaj0@gmail.com>
2023-09-03 01:20:36 -0400

    gitignore

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { register } from './controllers/auth.js'
 
 /** Configurations */
 
@@ -36,6 +37,10 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage })
+
+/** Routes */
+
+app.post("auth/register",upload.single())
 
 /** Mongoose Setup */
 const PORT = process.env.PORT || 6001;
